465872d8178f60aa906547d4f12087732a9eb08b
彭应浩 <729220650@qq.com>
2022-10-26 13:48:41 +0800

    feat(components): init

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    tabs: false,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    htmlWhitespaceSensitivity: 'css',
+    trailingComma: 'none',
+    useTabs: false,
+    jsxSingleQuote: false,
+    arrowParens: 'always',
+    rangeStart: 0,
+    proseWrap: 'always',
+    endOfLine: 'lf'
+}
---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+
+<template>vue3 + vite</template>
+<style scoped></style>
